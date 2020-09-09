e16c072538ae94365656ed4e2e00bac71c642d3f
pwnshell <65465491+pwnshell@users.noreply.github.com>
2020-09-10 02:39:51 +0530

    Update compute-offerings.py

--- compute-offerings.py
+++ compute-offerings.py
@@ -16,3 +16,7 @@ banner = pyfiglet.figlet_format("            Compute Offerings")
 print(termcolor.colored(banner, 'green', attrs=['bold']))
 for i, j, k in zip(x, y, z):
    subprocess.call(["cmk", "create", "serviceoffering", "name=" + str(i), "displaytext=" + str(i), "cpunumber=" + str(j), "cpuspeed=2000", "memory=" + str(k), "offerha=true", "tags=SSD", "hosttags=CIMI"])
+
+x.close()
+y.close()
+z.close()
