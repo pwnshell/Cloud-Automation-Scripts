c3c6d0815addeaf9d16658e6dd9dfc97123fbe31
pwnshell <65465491+pwnshell@users.noreply.github.com>
2020-09-10 12:29:53 +0530

    Update compute-offerings.py

--- compute-offerings.py
+++ compute-offerings.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
+
+#Author: Vijay Sachdeva
+#Script for creating n number of compute offerings
+
 import pyfiglet
 import subprocess
 import termcolor
-x = ["1C_1R", "2C_2R"]
-y = [1, 2]
-z = [1024, 2048]
+
+cname = open("/opt/name", "r")
+cpu = open("/opt/cpu", "r")
+ram = open("/opt/mem", "r")
+
+
 banner = pyfiglet.figlet_format("            Compute Offerings")
 print(termcolor.colored(banner, 'green', attrs=['bold']))
-for i, j, k in zip(x, y, z):
-   subprocess.call(["cmk", "create", "serviceoffering", "name=" + str(i), "displaytext=" + str(i), "cpunumber=" + str(j), "cpuspeed=2000", "memory=" + str(k), "offerha=true", "tags=SSD", "hosttags=CIMI"])
+for i, j, k in zip(cname, cpu, ram):
+        l=i.strip()
+        m=j.strip()
+        n=k.strip()
+        subprocess.call(["cmk", "create", "serviceoffering", "name=" + str(l), "displaytext=" + str(l), "cpunumber=" + str(m), "cpuspeed=2000", "memory=" + str(n), "offerha=true", "tags=SSD", "hosttags=CIMI"])
+
+
+cname.close()
+cpu.close()
+ram.close()
