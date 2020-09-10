fadca31e3ff4890f8a590c2360d98a7ae6c0f9ed
pwnshell <65465491+pwnshell@users.noreply.github.com>
2020-09-10 16:20:08 +0530

    Update compute-offerings.py

--- compute-offerings.py
+++ compute-offerings.py
@@ -11,15 +11,18 @@ cname = open("/opt/name", "r")
 cpu = open("/opt/cpu", "r")
 ram = open("/opt/mem", "r")
 
+try:
+        banner = pyfiglet.figlet_format("            Compute Offerings")
+        print(termcolor.colored(banner, 'green', attrs=['bold']))
+        for i, j, k in zip(cname, cpu, ram):
+                l=i.strip()
+                m=j.strip()
+                n=k.strip()
+                subprocess.call(["cmk", "create", "serviceoffering", "name=" + str(l), "displaytext=" + str(l), "cpunumber=" + str(m), "cpuspeed=2000", "memory=" + str(n), "offerha=true", "tags=SSD", "hosttags=CIMI"])
 
-banner = pyfiglet.figlet_format("            Compute Offerings")
-print(termcolor.colored(banner, 'green', attrs=['bold']))
-for i, j, k in zip(cname, cpu, ram):
-        l=i.strip()
-        m=j.strip()
-        n=k.strip()
-        subprocess.call(["cmk", "create", "serviceoffering", "name=" + str(l), "displaytext=" + str(l), "cpunumber=" + str(m), "cpuspeed=2000", "memory=" + str(n), "offerha=true", "tags=SSD", "hosttags=CIMI"])
-
+except KeyboardInterrupt:
+        print("\n")
+        print(termcolor.colored('\n[+] Ctrl+C..Bye..!!', 'red'))
 
 cname.close()
 cpu.close()
